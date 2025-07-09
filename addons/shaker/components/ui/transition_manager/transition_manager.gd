5096b3e90fad70f708cf49d8b29698a207ba6748
notgoyome <guillaume1.tran@epitech.eu>
2025-07-09 19:59:22 +0200

    fix: remove automatic scene change on ready

--- addons/shaker/components/ui/transition_manager/transition_manager.gd
+++ addons/shaker/components/ui/transition_manager/transition_manager.gd
@@ -1,9 +1,6 @@
 extends CanvasLayer
 @onready var animation_player = $AnimationPlayer
 
-func _ready() -> void:
-    change_scene(null)
-
 func change_scene(packed_scene: PackedScene) -> void:
     animation_player.play("transition")
     await get_tree().process_frame
